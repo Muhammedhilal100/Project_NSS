60ee5309f367eb498d461d5ad7fc392d111476c0
muhammed hilal <muhammedhilal100@gmail.com>
2023-03-15 16:52:29 +0530

    15/03/2023

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/index', { title: 'Express' });
+  res.render('home/index',{indexRoute:true});
+});
+router.get('/unicod_register', function(req, res, next) {
+  res.render('home/unicod_register',{indexRoute:true});
+});
+router.get('/po_register', function(req, res, next) {
+  res.render('home/po_register',{indexRoute:true});
+});
+router.get('/volunteer_register', function(req, res, next) {
+  res.render('home/volunteer_register',{indexRoute:true});
+});
+router.get('/suggestion', function(req, res, next) {
+  res.render('home/suggestion',{indexRoute:true});
+});
+router.get('/feedback', function(req, res, next) {
+  res.render('home/feedback',{indexRoute:true});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('home/login',{indexRoute:true});
 });
 
 module.exports = router;
